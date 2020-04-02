5f62feb5ef8e54ef9a40e2fe375df19af51ca70f
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2020-04-02 00:41:30 -0600

    field injection

--- app/src/main/java/com/example/daggercourse/MainActivity.kt
+++ app/src/main/java/com/example/daggercourse/MainActivity.kt
@@ -6,18 +6,21 @@ import com.example.daggercourse.di.ApiService
 import com.example.daggercourse.di.LocalStore
 import com.example.daggercourse.di.LoginManager
 import com.example.daggercourse.di.component.DaggerLoginComponent
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var loginManager: LoginManager
+    //field injection is useful when using 3rd party classes and things without constructor
+    // like activities
+    @Inject
+    lateinit var loginManager: LoginManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val loginComponent = DaggerLoginComponent.create()
-        loginManager = loginComponent.getLoginManager()
-
+        loginComponent.inject(this)
         loginManager.login("test","test")
 
     }
--- app/src/main/java/com/example/daggercourse/di/component/LoginComponent.kt
+++ app/src/main/java/com/example/daggercourse/di/component/LoginComponent.kt
@@ -1,11 +1,13 @@
 package com.example.daggercourse.di.component
 
+import com.example.daggercourse.MainActivity
 import com.example.daggercourse.di.LoginManager
 import dagger.Component
 
 
 @Component
 interface LoginComponent {
-    fun getLoginManager() : LoginManager
+    //fun getLoginManager() : LoginManager
+    fun inject(mainActivity: MainActivity)
 
 }
