34c1f9d1e31433574ea5b7dfe6b842c5e23bf68b
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2020-04-02 19:17:40 -0600

    addition of custom scope

--- app/src/main/java/com/example/daggercourse/di/component/AppComponent.kt
+++ app/src/main/java/com/example/daggercourse/di/component/AppComponent.kt
@@ -2,9 +2,11 @@ package com.example.daggercourse.di.component
 
 import com.example.daggercourse.di.AppLogger
 import com.example.daggercourse.di.module.AppModule
+import com.example.daggercourse.di.scope.AppScope
 import dagger.Component
 
 
+@AppScope
 @Component(modules = [AppModule::class])
 interface AppComponent{
 
--- app/src/main/java/com/example/daggercourse/di/module/AppModule.kt
+++ app/src/main/java/com/example/daggercourse/di/module/AppModule.kt
@@ -1,6 +1,7 @@
 package com.example.daggercourse.di.module
 
 import com.example.daggercourse.di.AppLogger
+import com.example.daggercourse.di.scope.AppScope
 import dagger.Module
 import dagger.Provides
 
@@ -8,7 +9,8 @@ import dagger.Provides
 class AppModule{
     var index = 0
 
-    @Provides
+    @Provides// does not recreates and the index stays at 1 because is made only once
+    @AppScope
     fun getAppLogger() : AppLogger{
         index++
         return AppLogger(index.toString())
